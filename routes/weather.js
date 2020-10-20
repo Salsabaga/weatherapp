aa358fa1eeaf55777bf79b79e7053e16af0ebb18
Salsabaga <dbaldeonabril@gmail.com>
2020-10-21 00:17:40 +0100

    Added Current Time and Sunset/Sunrise, some errors where full time not shown in some cities, must improve styling.

--- routes/weather.js
+++ routes/weather.js
@@ -2,12 +2,14 @@ const express = require("express");
 const fetch = require("node-fetch");
 let router = express.Router();
 
+
 router
 .route("/")
 .get((req,res)=>{
-    res.render("index",{message:null, city: null, weatherTemp:null, weatherDescrip:null, weatherWind:null, weatherIcon:null, weatherHumid:null});
+    res.render("index",{message:null, city: null, weatherTemp:null, weatherDescrip:null, weatherWind:null, weatherIcon:null, weatherHumid:null, sunrise:null, sunset:null, currentDay: null});
  
  });
+
  
  router
  .route("/")
@@ -26,13 +28,34 @@ router
              else{
                 const city = data.name;
                 const weatherTemp = data.main.temp;
-                const weatherDescrip = data.weather[0].description;
+                const weatherDescrip = data.weather[0].main;
                 const weatherWind = data.wind.speed;
                 const weatherHumid = data.main.humidity
                 const weatherIcon = data.weather[0].icon;
+                const weatherSunrise = data.sys.sunrise;
+                const weatherSunset = data.sys.sunset;
+
+                function getTime(weatherSun){
+                  let weatherTime = new Date (weatherSun * 1000);
+                  const hours = weatherTime.getUTCHours();
+                  const min = weatherTime.getUTCMinutes();
+                  return hours + ":" + min;
+                }
                
-                res.render("index",{city,weatherTemp,weatherDescrip,weatherWind,weatherIcon,weatherHumid});    
-             }
+                let today = new Date();
+                let options = {
+                   weekday: "long",
+                   day: "numeric",
+                   month: "long",
+                }               
+                let day = today.toLocaleDateString("en-US",options);
+ 
+                res.render("index",{city,weatherTemp,weatherDescrip,weatherWind,weatherIcon,weatherHumid,sunrise:getTime(weatherSunrise),sunset:getTime(weatherSunset),currentDay: day});   
+                }
+                
+               
+                 
+             
      
              
                               
